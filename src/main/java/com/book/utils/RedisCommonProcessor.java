8d31a070ea55af021581df9897d1c83c3444ff0c
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-05-24 09:45:20 +0800

    添加redis

---
+++ src/main/java/com/book/utils/RedisCommonProcessor.java
@@ -0,0 +1,33 @@
+package com.book.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisCommonProcessor {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public Object get(String key){
+        if(key == null){
+            throw new UnsupportedOperationException("Redis key could not be null");
+        }
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void set(String key,Object value){
+        redisTemplate.opsForValue().set(key,value);
+    }
+
+    public void set(String key,Object value,long timeout){
+        if(timeout > 0){
+            redisTemplate.opsForValue().set(key,value,timeout, TimeUnit.SECONDS);
+        }else {
+            set(key,value);
+        }
+    }
+}
